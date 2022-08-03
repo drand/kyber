bc98c414480ec8e073256e618f9036e146d4634a
Yolan Romailler <anomalroil@users.noreply.github.com>
2022-08-03 21:34:35 +0200

    adding xorSigma length test

--- encrypt/ibe/ibe_test.go
+++ encrypt/ibe/ibe_test.go
@@ -81,3 +81,16 @@ func TestVeryLongCipherFailsDecryptionBecauseOfLength(t *testing.T) {
 	require.Error(t, err)
 	require.ErrorContains(t, err, "ciphertext too long for the hash function provided")
 }
+
+func TestInvalidWFailsDecryptionBecauseOfLength(t *testing.T) {
+	suite, Ppub, ID, sQid := newSetting()
+	msg := []byte("hello world")
+	c, err := Encrypt(suite, Ppub, ID, msg)
+	require.NoError(t, err)
+
+	c.W = []byte(strings.Repeat("A", 25))
+	_, err = Decrypt(suite, sQid, c)
+
+	require.Error(t, err)
+	require.ErrorContains(t, err, "XorSigma is of invalid length")
+}
